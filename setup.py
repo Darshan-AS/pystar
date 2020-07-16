8357fd5450ae8c28450c8d5bbdea8d67cafb06bd
Darshan A S <itisdarshan@gmail.com>
2020-07-17 02:27:15 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pystar',
+    packages=['pystar'],
+    description='Machine Learning models',
+    version='0.1',
+    url='https://github.com/Darshan-AS/pystar.git',
+    author='Darshan A S',
+    author_email='itisdarshan@gmail.com',
+    install_requires=['numpy']
+)
